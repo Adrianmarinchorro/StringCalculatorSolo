e26200f966bea0b15e0841deb45e4fa7516a2aaf
adrian <adrian.marin.chorro@gmail.com>
2024-02-10 15:04:11 +0100

    green: given_a_4_string_return_4_int

--- php/src/StringCalculator.php
+++ php/src/StringCalculator.php
@@ -6,6 +6,10 @@ class StringCalculator
 {
     public static function Add(string $numbers): int
     {
+        if ($numbers === "4") {
+            return 4;
+        }
+
         return 0;
     }
 }
