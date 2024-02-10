db7ed664fed134fa81969a66c034d2c9eaf072c8
adrian <adrian.marin.chorro@gmail.com>
2024-02-10 15:31:27 +0100

    green: given_a_1_2_3_4_5_6_7_8_9_string_return_45_int

--- php/tests/StringCalculatorTest.php
+++ php/tests/StringCalculatorTest.php
@@ -38,4 +38,12 @@ class StringCalculatorTest extends TestCase
 
         self::assertEquals(6, $result);
     }
+
+    /** @test */
+    public function given_a_1_2_3_4_5_6_7_8_9_string_return_45_int(): void
+    {
+        $result = StringCalculator::Add("1,2,3,4,5,6,7,8,9");
+
+        self::assertEquals(45, $result);
+    }
 }
