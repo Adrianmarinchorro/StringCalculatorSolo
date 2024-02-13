116eeb8a9098795ddf7098ffadba6fb5de612792
adrian <adrian.marin.chorro@gmail.com>
2024-02-13 19:04:18 +0100

    refactor

--- php/src/StringCalculator.php
+++ php/src/StringCalculator.php
@@ -27,15 +27,33 @@ class StringCalculator
             return 3;
         }
 
-        $formattedNumbers = str_replace('\n', ',', $numbers);
-        $numbersArray = explode(',', $formattedNumbers);
+        $numbersArray = self::sanitizeString($numbers);
 
+        return self::sumNumbers($numbersArray);
+    }
+
+    /**
+     * @param array $numbersArray
+     * @return int
+     */
+    public static function sumNumbers(array $numbersArray): int
+    {
         $result = 0;
 
         foreach ($numbersArray as $number) {
-            $result += (int) $number;
+            $result += (int)$number;
         }
 
         return $result;
+    }
+
+    /**
+     * @param string $numbers
+     * @return string[]
+     */
+    public static function sanitizeString(string $numbers): array
+    {
+        $formattedNumbers = str_replace('\n', ',', $numbers);
+        return explode(',', $formattedNumbers);
     }
 }
