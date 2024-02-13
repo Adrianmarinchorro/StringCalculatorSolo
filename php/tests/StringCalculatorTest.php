dd3b972b03880e180d065206abf9491c0ccd7240
adrian <adrian.marin.chorro@gmail.com>
2024-02-13 19:23:08 +0100

    refactor: dataProvider

--- php/tests/StringCalculatorTest.php
+++ php/tests/StringCalculatorTest.php
@@ -7,91 +7,43 @@ use PHPUnit\Framework\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
-    /** @test */
-    public function given_a_empty_string_return_0(): void
-    {
-        $result = StringCalculator::Add("");
 
-        self::assertEquals(0, $result);
+    public static function simpleCalculator(): array
+    {
+        return [
+            'given_a_empty_string_return_0' => ["numbers" => '', "result" => 0],
+            'given_a_4_string_return_4_int' => ["numbers" => '4', "result" => 4],
+            'given_a_1_2_string_return_3_int' => ["numbers" => '1,2', "result" => 3],
+            'given_a_1_2_3_4_5_6_7_8_9_string_return_45_int' => ["numbers" => '1,2,3,4,5,6,7,8,9', "result" => 45],
+        ];
     }
 
-    /** @test */
-    public function given_a_4_string_return_4_int(): void
+    public static function simpleCalculatorWithNewLines(): array
     {
-        $result = StringCalculator::Add("4");
-
-        self::assertEquals(4, $result);
+        return [
+            'given_1_2_3_string_with_another_separator_return_6_int' => ["numbers" => '1\n2,3', "result" => 6],
+            'given_1_2_3_4_string_with_another_separator_return_10_int' => ["numbers" => '1\n2,3\n4', "result" => 10],
+            'given_1_2_3_4_5_string_with_another_separator_return_15_int' => ["numbers" => '1\n2,3\n4,5', "result" => 15],
+        ];
     }
 
-    /** @test */
-    public function given_a_1_2_string_return_3_int(): void
+    public static function simpleCalculatorWithPersonalizedSeparators(): array
     {
-        $result = StringCalculator::Add("1,2");
-
-        self::assertEquals(3, $result);
+        return [
+            'given_1_2_string_with_personalized_separator_return_3_int' => ["numbers" => '//;\n1;2', "result" => 3],
+            'given_1_2_3_string_with_personalized_separator_return_6_int' => ["numbers" => '//;\n1;2;3', "result" => 6],
+            'given_1_2_3_4_string_with_personalized_separator_return_10_int' => ["numbers" => '//_\n1_2_3_4', "result" => 10],
+        ];
     }
 
-    /** @test */
-    public function given_a_1_2_3_string_return_6_int(): void
+    /**
+     * @dataProvider simpleCalculator
+     * @dataProvider simpleCalculatorWithNewLines
+     * @dataProvider simpleCalculatorWithPersonalizedSeparators
+     * @test
+     */
+    public function given_a_string_then_return_the_sum(string $numbers, int $result): void
     {
-        $result = StringCalculator::Add("1,2,3");
-
-        self::assertEquals(6, $result);
-    }
-
-    /** @test */
-    public function given_a_1_2_3_4_5_6_7_8_9_string_return_45_int(): void
-    {
-        $result = StringCalculator::Add("1,2,3,4,5,6,7,8,9");
-
-        self::assertEquals(45, $result);
-    }
-
-    /** @test */
-    public function given_1_2_3_string_with_another_separator_return_6_int(): void
-    {
-        $result = StringCalculator::Add('1\n2,3');
-
-        self::assertEquals(6, $result);
-    }
-
-    /** @test */
-    public function given_1_2_3_4_string_with_another_separator_return_10_int(): void
-    {
-        $result = StringCalculator::Add('1\n2,3\n4');
-
-        self::assertEquals(10, $result);
-    }
-
-    /** @test */
-    public function given_1_2_3_4_5_string_with_another_separator_return_15_int(): void
-    {
-        $result = StringCalculator::Add('1\n2,3\n4,5');
-
-        self::assertEquals(15, $result);
-    }
-
-    /** @test */
-    public function given_1_2_string_with_personalized_separator_return_3_int(): void
-    {
-        $result = StringCalculator::Add('//;\n1;2');
-
-        self::assertEquals(3, $result);
-    }
-
-    /** @test */
-    public function given_1_2_3_string_with_personalized_separator_return_6_int(): void
-    {
-        $result = StringCalculator::Add('//;\n1;2;3');
-
-        self::assertEquals(6, $result);
-    }
-
-    /** @test */
-    public function given_1_2_3_4_string_with_personalized_separator_return_10_int(): void
-    {
-        $result = StringCalculator::Add('//_\n1_2_3_4');
-
-        self::assertEquals(10, $result);
+        $this->assertEquals($result, StringCalculator::Add($numbers));
     }
 }
